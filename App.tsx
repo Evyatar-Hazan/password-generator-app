e29d86c84975cd08b2ad793d120c29846f9b512f
Evyatar-Hazan <evyatarhazan3.14@gmail.com>
2023-08-29 09:57:19 +0300

    add input scrim

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@
  *
  * @format
  */
-
+import InputExample from './component/CreatePassword';
 import React from 'react';
 import type {PropsWithChildren} from 'react';
 import {
@@ -63,36 +63,38 @@ function App(): JSX.Element {
   };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+
+    <InputExample />
+    // <SafeAreaView style={backgroundStyle}>
+    //   <StatusBar
+    //     barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+    //     backgroundColor={backgroundStyle.backgroundColor}
+    //   />
+    //   <ScrollView
+    //     contentInsetAdjustmentBehavior="automatic"
+    //     style={backgroundStyle}>
+    //     <Header />
+    //     <View
+    //       style={{
+    //         backgroundColor: isDarkMode ? Colors.black : Colors.white,
+    //       }}>
+    //       <Section title="Step One">
+    //         Edit <Text style={styles.highlight}>App.tsx</Text> to change this
+    //         screen and then come back to see your edits.
+    //       </Section>
+    //       <Section title="See Your Changes">
+    //         <ReloadInstructions />
+    //       </Section>
+    //       <Section title="Debug">
+    //         <DebugInstructions />
+    //       </Section>
+    //       <Section title="Learn More">
+    //         Read the docs to discover what to do next:
+    //       </Section>
+    //       <LearnMoreLinks />
+    //     </View>
+    //   </ScrollView>
+    // </SafeAreaView>
   );
 }
 
---
+++ component/CreatePassword/index.tsx
@@ -0,0 +1,111 @@
+import React, { useState } from 'react';
+import { View, KeyboardAvoidingView, StyleSheet, ScrollView } from 'react-native';
+import { Input, Button } from 'react-native-elements';
+
+const InputExample = () => {
+  const [inputValue1, setInputValue1] = useState('');
+  const [inputValue2, setInputValue2] = useState('');
+
+  const handleInputChange1 = (text: string) => {
+    setInputValue1(text);
+  };
+
+  const handleInputChange2 = (text: string) => {
+    setInputValue2(text);
+  };
+
+  const isSaveDisabled = !inputValue1 || !inputValue2;
+
+  const handleSave = () => {
+    if (!isSaveDisabled) {
+      console.log('Input 1:', inputValue1);
+      console.log('Input 2:', inputValue2);
+    }
+  };
+
+  return (
+    <KeyboardAvoidingView behavior="padding" style={styles.container}>
+      <ScrollView
+        contentContainerStyle={styles.scrollContent}
+        keyboardShouldPersistTaps="handled"
+      >
+        <View style={styles.inputContainer}>
+          <Input
+            placeholder="Enter text first..."
+            placeholderTextColor="#aaa"
+            inputStyle={styles.input}
+            containerStyle={styles.inputWrapper}
+            inputContainerStyle={styles.inputContainerStyle} 
+            underlineColorAndroid="transparent"
+            onChangeText={handleInputChange1}
+            value={inputValue1}
+          />
+          <Input
+            placeholder="Insert second text..."
+            placeholderTextColor="#aaa"
+            inputStyle={styles.input}
+            containerStyle={styles.inputWrapper}
+            inputContainerStyle={styles.inputContainerStyle} 
+            underlineColorAndroid="transparent"
+            onChangeText={handleInputChange2}
+            value={inputValue2}
+          />
+          <Button
+            title="Create Password"
+            type="solid"
+            onPress={handleSave}
+            disabled={isSaveDisabled}
+            buttonStyle={[styles.saveButton, isSaveDisabled && styles.disabledButton]}
+            titleStyle={styles.saveButtonText}
+          />
+        </View>
+      </ScrollView>
+    </KeyboardAvoidingView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f0f0f0',
+  },
+  inputContainerStyle: {
+    borderBottomWidth: 0,
+  },
+  scrollContent: {
+    flexGrow: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+  },
+  inputContainer: {
+    width: '100%',
+    marginBottom: 20,
+    alignItems: 'center',
+  },
+  inputWrapper: {
+    marginBottom: 15,
+  },
+  input: {
+    fontSize: 16,
+    borderRadius: 25,
+    paddingHorizontal: 18,
+    backgroundColor: 'white',
+    elevation: 3,
+  },
+  saveButton: {
+    borderRadius: 25,
+    marginTop: 10,
+    backgroundColor: '#2ecc71',
+  },
+  disabledButton: {
+    backgroundColor: '#ccc',
+  },
+  saveButtonText: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+});
+
+export default InputExample;
