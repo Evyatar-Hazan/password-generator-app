e37a6f15026e82dd865895bb7c2c422b73a5ecff
Evyatar-Hazan <evyatarhazan3.14@gmail.com>
2023-09-19 19:28:43 +0300

    add copy button

--- component/outputScreen/outputContainer.tsx
+++ component/outputScreen/outputContainer.tsx
@@ -9,8 +9,7 @@ import {
 } from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
 import Clipboard from '@react-native-community/clipboard';
-
-
+import {Svg, Path} from 'react-native-svg';
 
 type GeneratedPasswords = {
   [key: string]: string;
@@ -18,13 +17,22 @@ type GeneratedPasswords = {
 
 type PasswordType = keyof GeneratedPasswords;
 
-const mapGeneratedPasswords: Record<PasswordType,{label: string; securityLevel: string}> = {
-    numbersPassword: {label: 'Numbers only:', securityLevel: 'Low'},
-    lettersPassword: {label: 'Letters only:', securityLevel: 'Medium'},
-    numbersAndLettersPassword: {label: 'Numbers and letters:', securityLevel: 'Medium'},
-    upperCasePassword: {label: 'Including capital letters:', securityLevel: 'High'},
-    transformToSign: {label: 'Including a sign:', securityLevel: 'Very High'},
-  };
+const mapGeneratedPasswords: Record<
+  PasswordType,
+  {label: string; securityLevel: string}
+> = {
+  numbersPassword: {label: 'Numbers only:', securityLevel: 'Low'},
+  lettersPassword: {label: 'Letters only:', securityLevel: 'Medium'},
+  numbersAndLettersPassword: {
+    label: 'Numbers and letters:',
+    securityLevel: 'Medium',
+  },
+  upperCasePassword: {
+    label: 'Including capital letters:',
+    securityLevel: 'High',
+  },
+  transformToSign: {label: 'Including a sign:', securityLevel: 'Very High'},
+};
 
 const getSecurityLevelColor = (securityLevel: string) => {
   const colorsMap: {[key: string]: string} = {
@@ -40,21 +48,22 @@ const getSecurityLevelColor = (securityLevel: string) => {
   }
 };
 
-const copyToClipboard = (generatedPasswords: GeneratedPasswords, passwordType: string) => {
-    Clipboard.setString(generatedPasswords[passwordType]);
-    ToastAndroid.show(
-      `Copied ${mapGeneratedPasswords[passwordType].label} to clipboard`,
-      ToastAndroid.SHORT,
-    );
-  };
+const copyToClipboard = (
+  generatedPasswords: GeneratedPasswords,
+  passwordType: string,
+) => {
+  Clipboard.setString(generatedPasswords[passwordType]);
+  ToastAndroid.show(
+    `Copied ${mapGeneratedPasswords[passwordType].label} to clipboard`,
+    ToastAndroid.SHORT,
+  );
+};
 
 const OutputContainer = ({
-    generatedPasswords,
+  generatedPasswords,
 }: {
-    generatedPasswords: GeneratedPasswords;
+  generatedPasswords: GeneratedPasswords;
 }) => {
-
-
   return (
     <ScrollView style={styles.outputContainer}>
       {Object.keys(generatedPasswords).map((passwordType: string) => (
@@ -81,11 +90,23 @@ const OutputContainer = ({
               <Text style={styles.output}>
                 {generatedPasswords[passwordType]}
               </Text>
-              <TouchableOpacity
+              <Svg
+                height="16"
+                width="16"
+                viewBox="0 0 16 16"
                 style={styles.copyButton}
-                onPress={() => copyToClipboard(generatedPasswords, passwordType)}>
-                <Text style={styles.copyButtonText}>Copy</Text>
-              </TouchableOpacity>
+                onPress={() =>
+                  copyToClipboard(generatedPasswords, passwordType)
+                }>
+                <Path
+                  d="M0 6.75C0 5.784.784 5 1.75 5h1.5a.75.75 0 0 1 0 1.5h-1.5a.25.25 0 0 0-.25.25v7.5c0 .138.112.25.25.25h7.5a.25.25 0 0 0 .25-.25v-1.5a.75.75 0 0 1 1.5 0v1.5A1.75 1.75 0 0 1 9.25 16h-7.5A1.75 1.75 0 0 1 0 14.25Z"
+                  fill="black"
+                />
+                <Path
+                  d="M5 1.75C5 .784 5.784 0 6.75 0h7.5C15.216 0 16 .784 16 1.75v7.5A1.75 1.75 0 0 1 14.25 11h-7.5A1.75 1.75 0 0 1 5 9.25Zm1.75-.25a.25.25 0 0 0-.25.25v7.5c0 .138.112.25.25.25h7.5a.25.25 0 0 0 .25-.25v-7.5a.25.25 0 0 0-.25-.25Z"
+                  fill="black"
+                />
+              </Svg>
             </View>
           </LinearGradient>
         </View>
@@ -96,11 +117,11 @@ const OutputContainer = ({
 
 const styles = StyleSheet.create({
   copyButton: {
-    backgroundColor: 'grey',
-    paddingVertical: 8,
-    paddingHorizontal: 16,
+    padding: 8,
     borderRadius: 8,
     marginLeft: 100,
+    borderWidth: 2,
+    borderColor: 'black',
   },
   copyButtonText: {
     fontSize: 16,
