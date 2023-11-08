7d9c49e2eb7d58da993257975fc1a311f4f40a9a
Evyatar-Hazan <evyatarhazan3.14@gmail.com>
2023-11-08 21:27:57 +0200

    fix RTL

--- App.tsx
+++ App.tsx
@@ -1,16 +1,39 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Main from './src/main';
 import * as Sentry from '@sentry/react-native';
 import SplashScreen from 'react-native-splash-screen';
+import i18n from './src/component/languageHandling/i18next.config';
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import {I18nManager} from 'react-native';
 
 Sentry.init({
   dsn: 'https://61da0d0eb38990802eb1d1919fef4f54@o4506066093801472.ingest.sentry.io/4506066096685056',
 });
 
 const App: React.FC = () => {
-  React.useEffect(() => {
-    SplashScreen.hide();
-  }, []);
+  const STORE_LANGUAGE_KEY = "settings.lang";
+  const [currentLanguage, setCurrentLanguage] = useState(i18n.language);
+
+  
+  useEffect(() => {
+    const setLanguage = async () => {
+      const language = await AsyncStorage.getItem(STORE_LANGUAGE_KEY)
+      setCurrentLanguage(language || currentLanguage)
+    
+      const isHebrew = language === 'he' || (language === null && currentLanguage === 'he');
+    
+      try {
+        I18nManager.allowRTL(isHebrew); 
+        i18n.changeLanguage(language || currentLanguage);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+    setLanguage()
+    setTimeout(() => {
+      SplashScreen.hide();
+    }, 2000);
+  }, [currentLanguage]);
 
   return <Main />;
 };
--- android/app/src/main/java/com/passwordgenerator/MainApplication.java
+++ android/app/src/main/java/com/passwordgenerator/MainApplication.java
@@ -57,7 +57,6 @@ public class MainApplication extends Application implements ReactApplication {
   public void onCreate() {
     super.onCreate();
     I18nUtil sharedI18nUtilInstance = I18nUtil.getInstance();
-    sharedI18nUtilInstance.allowRTL(getApplicationContext(), true);
     SoLoader.init(this, /* native exopackage */ false);
     if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
       // If you opted-in for the New Architecture, we load the native entry point for this app.
