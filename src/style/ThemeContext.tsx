075a0ad6ae0a4134f413be1691a14346294fdd74
Evyatar-Hazan <evyatarhazan3.14@gmail.com>
2023-11-28 23:08:02 +0200

    add colorScheme to AsyncStorage

--- src/style/ThemeContext.tsx
+++ src/style/ThemeContext.tsx
@@ -1,4 +1,12 @@
-import React, {createContext, useContext, useState, ReactNode} from 'react';
+import React, {
+  createContext,
+  useContext,
+  useState,
+  useEffect,
+  ReactNode,
+} from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {useColorScheme} from 'react-native';
 
 interface ThemeContextProps {
   colorScheme: string;
@@ -21,10 +29,31 @@ interface ThemeProviderProps {
 
 export const ThemeProvider: React.FC<ThemeProviderProps> = ({children}) => {
   const [colorScheme, setColorScheme] = useState<string>('light');
+  const scheme = useColorScheme();
+
+  useEffect(() => {
+    const loadColorScheme = async () => {
+      try {
+        const savedColorScheme = await AsyncStorage.getItem('colorScheme');
+        if (savedColorScheme) {
+          setColorScheme(savedColorScheme);
+        } else {
+          setColorScheme(scheme || 'light');
+        }
+      } catch (error) {
+        console.error('Error loading colorScheme from AsyncStorage:', error);
+      }
+    };
+
+    loadColorScheme();
+  }, []);
 
   const toggleColorScheme = () => {
     const newScheme = colorScheme === 'light' ? 'dark' : 'light';
     setColorScheme(newScheme);
+    AsyncStorage.setItem('colorScheme', newScheme).catch(error => {
+      console.error('Error saving colorScheme to AsyncStorage:', error);
+    });
   };
 
   const theme: ThemeContextProps = {
